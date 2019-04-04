067fe388adb76299b8004e02156dda4bbd502ce5
Alan Cameron Wills <alan@cameronwills.org>
2019-04-04 15:29:56 +0100

    Single place search returns home zone

--- place/index.js
+++ place/index.js
@@ -1,8 +1,18 @@
+var ZoneFilter=require("../Shared/ZoneFilter.js");
+
+// Find a single place, given its id
 module.exports = function (context, req, intable) {
-    context.log("Retrieved records:", intable.length);
+    //context.log("Retrieved records:", intable.length);
     var outtable = intable.filter(x => x.RowKey == req.query.id);
+
+    if (outtable.length > 0){
+        // Append a zone that the place can be found in:
+        var zf = new ZoneFilter(null);
+        outtable[0].HomeZone = zf.ok(outtable[0]);
+    }
+
     context.res = {
-        status: 200,
+        status: outtable.length > 0 ? 200 : 400,
         body: outtable
     };
     context.done();
