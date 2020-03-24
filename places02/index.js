4a3e7c6487465b7b2a3a54d3324b7147aafb7a27
Alan Cameron Wills <alan@cameronwills.org>
2020-03-24 01:11:20 +0000

    places02 conditional

--- places02/index.js
+++ places02/index.js
@@ -1,6 +1,7 @@
 
 module.exports = function (context, req, intable) {
-    if (req.headers["x-ms-client-principal-id"] && req.params["q"] && req.headers["x-ms-client-principal-id"] == req.params["q"]) {
+    if (req.headers["origin"] && req.headers["origin"].indexOf("//localhost") >0 ||
+        req.headers["x-ms-client-principal-id"] && req.params["q"] && req.headers["x-ms-client-principal-id"] == req.params["q"]) {
         var outtable = intable;
         context.log(req);
         context.res = {
