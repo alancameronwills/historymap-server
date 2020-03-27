98ed454931c8f5b5e26868a0c39060a9fb0a3cb5
Alan Cameron Wills <alan@cameronwills.org>
2020-03-27 21:19:36 +0000

    private check

---
+++ private/index.js
@@ -0,0 +1,13 @@
+module.exports = function (context, req) {
+        
+    var names = ["pantywlan@gmail.com", "Sue Sturges"];
+    var name = req.Headers["x-ms-client-principal-name"] || "";
+    var ok = names.indexOf(name) >=0;
+        
+        context.res = {
+            status: ok? 200 : 401,
+            body: name
+        };
+    
+    context.done();
+};
