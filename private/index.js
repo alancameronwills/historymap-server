4d56b9485cf02167f615fab9d25d5ec9c2b20e9e
Alan Cameron Wills <alan@cameronwills.org>
2020-03-27 21:53:43 +0000

    fix

--- private/index.js
+++ private/index.js
@@ -6,7 +6,7 @@ module.exports = function (context, req) {
     //context.log(req);
     var ok = names.indexOf(name) >=0;
     //context.log(ok);
-    //context.log(name);
+    context.log(name);
         context.res = {
             status: ok? 200 : 401,
             body: name
