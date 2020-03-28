c60897e0480ac24fdc5f0e00a29318326d5b15a2
Alan Cameron Wills <alan@cameronwills.org>
2020-03-28 21:42:53 +0000

    fix

--- private/index.js
+++ private/index.js
@@ -1,6 +1,6 @@
 module.exports = function (context, req) {
         
-    var names = ["pantywylan@gmail.com", "sue.sturges@btconnect.com"];
+    var names = ["pantywylan@gmail.com", "Sue.sturges@btconnect.com"];
     
     var name = req.headers["x-ms-client-principal-name"] || "";
     //context.log(req);
